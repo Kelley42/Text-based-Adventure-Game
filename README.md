51aeb8fe195dced02224e971d9855679c1c8fb2e
Kelley42 <kelleykristin42@yahoo.com>
2022-04-05 17:47:14 -0500

    edited README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# README.md# Text-based-Adventure-Game
+# Adventure Game
+
+This is a game I wrote for Udemy's 100 Days of Code. It's a command-line program written in Python. It takes you on an adventure through a cursed island to find a treasure.
+
+I had so much fun creating it! I've always wanted to write something like this. The original storyline structure we were invited to model ours after only had 3 steps, so I decided to add a lot more to make it feel like an actual story.
+
+It was important to me to make it feel as old-school as possible, so I didn't want users to input a letter or number as their choice but to actually type their response as a word or phrase. While it was more difficult this way and involved more lines of code to cover all likely responses they might give, I'm happy with the result.
+
+While the original 3-step model had each GAME OVER branch completely ending the game, I wanted to make it less frustrating so users wouldn't have to start all the way at the beginning each time they died in the game. (Though this could be removed to make the game more difficult.) Instead it would offer them a choice to either start over from the beginning or just to revert to the last step before their death so they can choose a new action. However, they can only do this twice - on their third GAME OVER, they must start from the beginning.
+
+While creating this game, organization of each section was the most difficult and something I rearranged many times. It was also difficult tracking all the branches within the story. Figuring out how to error-check all of their typed responses was also a good learning experience.
+
+
+
+
--- adventure.py
+++ adventure.py
@@ -23,12 +23,39 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.\n")
 print("Your mission is to find the treasure.\n") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-#by SWEptaway
-#Write your code below this line 👇
+#defs
+def beach():
+    beach_response = input("What do you do? Run into the water, check out the bushes, or stay put?\n").lower()
 
-
+    beach_answer1 = [
+        "run into the water",
+        "run",
+        "water"
+    ]
+    beach_answer2 = [
+        "check out the bushes",
+        "check",
+        "bushes"
+    ]
+    beach_answer3 = [
+    "stay put",
+    "stay",
+    "put"
+    ]
+  
+    if beach_response in beach_answer1:
+        print("Oh, no! A very large sea monster has eaten you. That's too bad.\nGAME OVER")
+        start_over()
+        return 1
+    elif beach_response in beach_answer2:
+        see_shape()
+    elif beach_response in beach_answer3:
+        hungry()
+    else:
+        print("!!!Incorrect response - try again!!!")
+        beach()
 
 #Intro
 print("     A storm has shipwrecked you and your crew on a tropical island. There are rumors that the island had a terrible curse put upon it long ago. Numerous treasure ships have mysteriously sunk along its coast, and the few who survived insisted that anyone who stepped foot on land was never seen again.\n     Night has fallen and you have woken up on the beach alone in the cold rain. You can't see or hear any of your crew members. Cold and wet, you can't stop shivering. Suddenly you hear rustling in the bushes behind you.\n")
 
+beach()
